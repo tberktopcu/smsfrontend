2c4d6f4b7112fc9ffae483f4d72d9ba4d5d1f9cd
Tahir Berk <tahirberktopcu@gmail.com>
2025-06-17 14:23:16 +0300

    düzeltme

--- src/SmsManager.jsx
+++ src/SmsManager.jsx
@@ -1,6 +1,6 @@
 ﻿import React, { useEffect, useState } from "react";
 
-const API_BASE_URL = "https://smsbackend-t1kx.onrender.com";
+const API_BASE_URL = "https://smsbackend-t1kx.onrender.com/api";
 
 export default function SmsManager() {
     const [infos, setInfos] = useState([]);
